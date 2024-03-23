da097397462afa7e95564e31f1d0febf6cf09e70
Alexiotovv <gpsalexvasquez@gmail.com>
2024-03-23 17:28:31 -0500

    primerasubida

---
+++ app/Http/Controllers/ProductoController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\producto;
+use Illuminate\Http\Request;
+
+class ProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        
+        $producto = new producto();
+        $producto->nombre = $request->input('Nombre');
+        $producto->descripcion = $request->input('Descripcion');
+        $producto->precio = $request->input('Precio');
+        $producto->save();
+        return response()->json([ 'data'=>$producto, 'response'=>'Succesfully Registered' ], 200);
+
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, producto $producto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(producto $producto)
+    {
+        //
+    }
+}
